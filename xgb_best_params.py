bf3411360b495f4d072fe0dfd44c896cd830f2e7
MaxBodley <mbodley657@gmail.com>
2023-08-24 15:32:55 -0400

    optimized analysis

---
+++ xgb_best_params.py
@@ -0,0 +1,13 @@
+"""
+Saving this data outside notebook because it took 2.5 hours to obtain.
+Used for redundancy along with xgboost_best_model.model. Don't want to lose it!
+"""
+
+best_params={'eta': 0.15162209903799176,
+             'max_depth': 137,
+             'subsample': 0.7901873002755387,
+             'colsample_bytree': 0.7942990466979036,
+             'alpha': 0.5202457506105307,
+             'lambda': 2.0031308603814093,
+             'gamma': 0.6452736868434039,
+             'min_child_weight': 3.1547669765333395}
